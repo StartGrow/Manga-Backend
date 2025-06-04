adb6e8d5de55fd2e3c9ae0da38953b92f1d5477b
StartGrow <132191403+StartGrow@users.noreply.github.com>
2025-06-04 22:07:55 +0700

    Update chapter.js

--- routers/chapter.js
+++ routers/chapter.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const cheerio = require("cheerio");
-const axios = require("axios").default
+const axios = require("axios").default;
 const AxiosService = require("../helpers/axiosService");
 
 router.get("/", (req, res) => {
@@ -9,49 +9,42 @@ router.get("/", (req, res) => {
   });
 });
 
-//chapter ----done ----
+// chapter endpoint
 router.get("/:slug", async (req, res) => {
   const slug = req.params.slug;
   try {
-    const response = await AxiosService(`ch/${slug}/`);
-    // const response = await axios.get(`https://komikcast.id/${slug}`)
+    const response = await AxiosService(`https://komiku.org/${slug}/`);
     const $ = cheerio.load(response.data);
-    const content = $("#article");
-    let chapter_image = [];
-    const obj = {};
-    obj.chapter_endpoint = slug + "/";
-    obj.chapter_name = slug.split('-').join(' ').trim()
+    
+    const obj = {
+      chapter_endpoint: slug + "/",
+      chapter_name: slug.split('-').join(' ').trim(),
+      title: "",
+      chapter_pages: 0,
+      chapter_image: []
+    };
 
-    obj.title = $('#Judul > header > p > a > b').text().trim()
-    /**
-     * @Komiku
-     */
-    const getTitlePages = content.find(".dsk2")
-    getTitlePages.filter(() => {
-      obj.title = $(getTitlePages).find("h1").text().replace("Komik ", "");
-    });
-
-    /**
-     * @Komiku
-     */
-    const getPages = $('#Baca_Komik > img')
-
-    // const getPages = $('#chimg > img')
-    obj.chapter_pages = getPages.length;
-    getPages.each((i, el) => {
-      chapter_image.push({
-        chapter_image_link: $(el).attr("src").replace('i0.wp.com/',''),
-        image_number: i + 1,
+    // Get all chapter images if they exist
+    const getPages = $('#Baca_Komik img');
+    if (getPages.length > 0) {
+      obj.chapter_pages = getPages.length;
+      getPages.each((i, el) => {
+        obj.chapter_image.push({
+          chapter_image_link: $(el).attr("src"),
+          image_number: i + 1,
+        });
       });
-    });
-    obj.chapter_image = chapter_image;
+    }
+
     res.json(obj);
   } catch (error) {
     console.log(error);
-    res.send({
-      status: false,
-      message: error,
-      chapter_image :[]
+    res.json({
+      chapter_endpoint: slug + "/",
+      chapter_name: slug.split('-').join(' ').trim(),
+      title: "",
+      chapter_pages: 0,
+      chapter_image: []
     });
   }
 });
